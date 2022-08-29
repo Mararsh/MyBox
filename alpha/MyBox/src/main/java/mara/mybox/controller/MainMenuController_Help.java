cc63a702ac4c798afdb2ad93ad511c0ebc005108
Mara <rshmara@sina.com>
2022-08-29 20:37:34 +0800

    a6.5.9

--- alpha/MyBox/src/main/java/mara/mybox/controller/MainMenuController_Help.java
+++ alpha/MyBox/src/main/java/mara/mybox/controller/MainMenuController_Help.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import mara.mybox.fxml.ControllerTools;
 import mara.mybox.fxml.FxFileTools;
+import mara.mybox.fxml.PopTools;
 import mara.mybox.value.Fxmls;
 import mara.mybox.value.Languages;
 
@@ -14,6 +15,15 @@ import mara.mybox.value.Languages;
  * @License Apache License Version 2.0
  */
 public abstract class MainMenuController_Help extends MainMenuController_Development {
+
+    @FXML
+    protected void Overview(ActionEvent event) {
+        String lang = Languages.isChinese() ? "zh" : "en";
+        File file = FxFileTools.getInternalFile("/doc/MyBox-Overview-" + lang + ".pdf", "doc", "MyBox-Overview-" + lang + ".pdf");
+        if (file != null && file.exists()) {
+            PopTools.browseURI(this, file.toURI());
+        }
+    }
 
     @FXML
     protected void Shortcuts(ActionEvent event) {
